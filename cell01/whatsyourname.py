3580138d421a7285bcc9504f2743ed12559dfbbe
atirasir <agpzxq@gmail.com>
2026-04-25 05:53:08 +0000

    2.0

--- cell01/whatsyourname.py
+++ cell01/whatsyourname.py
@@ -1,3 +1,3 @@
 fn = input("Hey, what's your first name? : ")
 ln = input("And your last name? : ")
-print ("Well, pleased to meet you," + " " + Fn + " " + ln)
+print ("Well, pleased to meet you," + " " + fn + " " + ln)
